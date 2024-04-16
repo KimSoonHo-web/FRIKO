5c72ceed2714c8f3693e21533835217484813402
rlatnsgh25 <rlatnsgh25@naver.com>
2024-04-16 20:32:44 +0900

    포로젝트 촉기화

---
+++ src/main/java/com/owol/friko/FrikoApplication.java
@@ -0,0 +1,13 @@
+package com.owol.friko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrikoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrikoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/owol/friko/FrikoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owol.friko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrikoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
